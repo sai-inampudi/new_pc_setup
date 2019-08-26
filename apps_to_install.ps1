3926c6760db15a538b1db458c278c7ea11fb3194
sai-inampudi <46006458+sai-inampudi@users.noreply.github.com>
2019-08-26 15:17:02 -0500

    Create apps_to_install.ps1

---
+++ apps_to_install.ps1
@@ -0,0 +1,26 @@
+# Simple environment setup script
+choco install 7zip
+choco install nodejs.install
+choco install python3
+choco install ruby
+choco install git
+choco install putty
+
+# IDE
+choco install notepadplusplus
+choco install intellijidea-community
+choco install webstorm
+
+# Media
+choco install spotify
+choco install vlc
+
+# Optional
+choco install copyq #clipboard history manager
+choco install keypirinha #keystroke application launcher, similar to alfred/spotlight search on Mac
+choco install autohotkey #Allows you to run scripts tied to shortcuts
+choco install postman #Tool for hitting HTTP APIs
+choco install cmder #console emulator, provides features such as tabbing in a console window
+choco install curl #CLI to exchange requests/responses with a server
+choco install wget #CLI to download files from servers
+choco install agentransack #Utility that helps find files on your PC or network
